a17f27e581f09ad0843398f14a96115f9adf39c7
Jawahar Rajan <jawaharrajan@gmail.com>
2019-08-13 23:26:43 -0400

    day 5 update

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,15 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let nReturn: number = 0;
+    let inputArrayLength: number = inputArray.length;
+    let nRet: number[] = []
 
+    for(var i = 0; i < inputArray.length;i++){
+        nRet[i] = inputArray[i] * inputArray[i+1];
+        //console.log(nRet);
+        nRet.sort((n1,n2) => n2 - n1);
+        //console.log(nRet);
+    }
+    return nRet[0];
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
