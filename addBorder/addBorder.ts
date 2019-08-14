8fa988991226397417955a6bb62b2e4417a97612
Jawahar Rajan <jawaharrajan@gmail.com>
2019-08-13 22:10:26 -0400

    Day 1 to 4 update

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 function addBorder(picture: string[]): string[] {
+    
+    for(var i = 0; i < picture.length; i++) {
+        picture[i] = "*".concat(picture[i]);
+        picture[i] = picture[i].concat("*");
+        
+        console.log(picture[i]);
+    };
+    picture.unshift("*".repeat(picture.length + 3));
+    picture.push("*".repeat(picture.length + 3));
 
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,13 @@
 function addTwoDigits(n: any): number {
 
+    let s: string = n.toString();
+    let ss: string[] = s.split("");
+    let nReturn: number =0;
+
+    ss.forEach((el)=>{
+        nReturn += parseInt(el);
+    });
+    return nReturn;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(99));
