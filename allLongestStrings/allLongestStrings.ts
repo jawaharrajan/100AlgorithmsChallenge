449778c8cc90307ab3fc04ffab6481b784423fe0
Jawahar Rajan <jawaharrajan@gmail.com>
2019-08-18 10:51:08 -0400

    day 6 to 8

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    let strRet: string[] = [];
+    let longestString: number = 0;
 
+    inputArray.forEach((word: string)=>{
+        longestString = longestString < word.length ? word.length : longestString;
+    });
+
+    inputArray.forEach((word: string)=>{
+        if (word.length === longestString){
+            strRet.push(word);
+            //console.log(strRet);
+        }        
+    });
+    return strRet;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,17 @@
 function alphabeticShift(inputString: string): string {
+    let strToReturn: string = "";
+  
+    inputString.split("").forEach((letter: string)=>{
+        //console.log(letter.charAt(0));
+        //console.log(letter.charCodeAt(0));
+        //console.log(letter.charCodeAt(0) + 1);
+        //console.log(String.fromCharCode(letter.charCodeAt(0) + 1));
 
+        strToReturn += (letter.charAt(0) === "z" ? "a" :
+            String.fromCharCode(letter.charCodeAt(0) + 1));
+    });
+
+    return strToReturn;
 }
 
 console.log(alphabeticShift('crazy'));
