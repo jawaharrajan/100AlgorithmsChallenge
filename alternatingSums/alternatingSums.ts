3df75b8a9007b0956db53713c65fdabf9809e6e5
Jawahar Rajan <jawaharrajan@gmail.com>
2019-08-19 22:46:56 -0400

    day 9 and 10

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,21 @@
 function alternatingSums(a: number[]): number[] {
+    let retVal: number[] = [];
+    let atot: number = 0;
+    let btot: number = 0;
 
+    for(let i = 0; i < a.length; i++){
+        if (i % 2 !== 0){
+            atot += a[i];
+            console.log(atot);
+        } else {
+            btot+= a[i];
+            console.log(btot);
+        }
+    }
+    retVal.push(btot);
+    retVal.push(atot);
+    return retVal;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([1,2,3,4,5,6,7,8,9]))
